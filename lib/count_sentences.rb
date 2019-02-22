ea3847886c051fdbdf3c6eb5fe4454990052588d
Kelsey Low <kklowlow@gmail.com>
2019-02-22 05:04:47 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -53,31 +53,6 @@ end
 =begin
 
 class String
-
-  def sentence?
-    if self.end_with?(".") == true 
-      return true 
-    else 
-      return false
-    end
-  end
-
-  def question?
-    if self.end_with?("?") == true 
-      return true 
-    else 
-      return false
-    end
-  end
-
-  def exclamation?
-    if self.end_with?("!") == true 
-      return true 
-    else 
-      return false
-    end
-  end
-
   def count_sentences
     new_array = self.split(/[.?!]/).count
     return new_array
